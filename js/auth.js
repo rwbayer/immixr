72a1316f5fc217e432554b62bd8f362b9b8b54c1
Robert Bayer <robertwbayer@gmail.com>
2015-11-28 01:52:12 -0600

    updating url interpolation

--- js/auth.js
+++ js/auth.js
@@ -12,8 +12,8 @@ jQuery(function($) {
 		loaded = true;
 		Adjust();
 		var url = window.location.href; 
-		url.split("=");
-		console.log(url[1]);
+		var accessToken = getUrlParameter('access_token');
+		console.log(accessToken);
 		
 	});
 
@@ -50,6 +50,22 @@ jQuery(function($) {
 
 		});
 	}
+
+// FROM: http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
 	
 
 
