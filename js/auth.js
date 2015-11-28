a06453a40b2c873b7d5e5385d426163f1ae7b6b5
Robert Bayer <robertwbayer@gmail.com>
2015-11-28 02:06:57 -0600

    updating url interpolation

--- js/auth.js
+++ js/auth.js
@@ -13,7 +13,7 @@ jQuery(function($) {
 		Adjust();
 		var url = window.location.href;
 		console.log(url); 
-		var accessToken = $.urlParam('access_token');
+		var accessToken = url.substring(54,70);
 		console.log(accessToken);
 		
 	});
@@ -53,15 +53,15 @@ jQuery(function($) {
 	}
 
 // FROM: http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
-$.urlParam = function(name){
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results==null){
-       return null;
-    }
-    else{
-       return results[1] || 0;
-    }
-}
+// $.urlParam = function(name){
+//     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+//     if (results==null){
+//        return null;
+//     }
+//     else{
+//        return results[1] || 0;
+//     }
+// }
 	
 
 
